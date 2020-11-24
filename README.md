cd2e3d655cb63cfe5088469bbc252017ee5558ca
abhik1505040 <1505040.ab@ugrad.cse.buet.ac.bd>
2020-11-24 22:11:57 +0600

    Updated drive link

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 # installs required libraries and downloads raw data
 chmod +x ./configure.sh && ./configure.sh
 ```
+**For python 3.7, you may need to do ```pip uninstall typing``` if this script throws registry related errors.**
 
 ## Usage 
 
--- configure.sh
+++ configure.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # install modified label-studio
-# pip3 install --upgrade ./
+pip uninstall label_studio -y
+pip install -r requirements.txt
+python setup.py install
 
 # download data from drive
-# gdrive_id="1DXqd45SKV-jP4nou0CMOJPwSO3w-0UFl"
+gdrive_id="17z0eCVcogSFiWyZmnFgw9X1UnD4kNh8_"
 out_name="bbc_data.json"
-# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HlzTR1-YVoBPlXo0gMFJ_xY4ogMnfzDi' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${gdrive_id}" -O ${out_name} && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HlzTR1-YVoBPlXo0gMFJ_xY4ogMnfzDi' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${gdrive_id}" -O ${out_name} && rm -rf /tmp/cookies.txt
 
 # set up project
 label-studio init "summary_annotation" -b \
